939d7a2b2b7620a8b4b99fd6ca1e6e7f29963575
doraeza <pp8960@naver.com>
2023-06-16 18:52:11 +0900

    base structure6

---
+++ portfolio/src/components/layout/desktop.layout.tsx
@@ -0,0 +1,20 @@
+// src/layouts/desktop_layout.js
+import Body from "@components/body"
+import Footer from "@components/footer"
+import Header from "@components/header"
+import React from "react"
+
+const DesktopLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <>
+            <div className="flex flex-col justify-between">
+                <Header />
+                <Body>{children}</Body>
+                <div></div>
+            </div>
+            <Footer />
+        </>
+    )
+}
+
+export default DesktopLayout
--- portfolio/src/components/layout/index.tsx
+++ portfolio/src/components/layout/index.tsx
@@ -1,6 +1,28 @@
-import React from 'react';
+// import Aside from "@components/aside";
+import React from "react"
+import MobileLayout from "./mobile.layout"
+import DesktopLayout from "./desktop.layout"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  
-  return <div className="relative  top-10">{children}</div>;
+    
+    const Desktop = () => {
+        const isDesktop = useMediaQuery({ minWidth: 768 })
+        if (isDesktop) {
+            return <DesktopLayout>{children}</DesktopLayout>
+        }
+    }
+
+    const Mobile = () => {
+        const isMobile = useMediaQuery({ maxWidth: 767 })
+        if (isMobile) {
+            return <MobileLayout>{children}</MobileLayout>
+        }
+    }
+
+    return (
+        <div>
+            {Desktop && Desktop()}
+            {Mobile && Mobile()}
+        </div>
+    )
 }
