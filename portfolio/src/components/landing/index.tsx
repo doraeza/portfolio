0c8b670db9369dd40252cd57ab810539646e31d1
doraeza <pp8960@naver.com>
2023-06-20 22:23:17 +0900

    intro1

--- portfolio/src/components/landing/index.tsx
+++ portfolio/src/components/landing/index.tsx
@@ -6,6 +6,7 @@ const Landing = () => {
     return (
         <>
         <div className='fixed right-10 bottom-12 cursor-pointer'><TopButton/></div>
+        <div>asfdsfasdfqwrqw123123</div>
         </>
     )
 }
--- portfolio/src/pages/App.tsx
+++ portfolio/src/pages/App.tsx
@@ -1,12 +1,14 @@
 import CustomButton from '../components/common/CustomButton';
 import '../styles/globals.css';
 
-
 function App() {
   return (
       // <AppRouter />
       <>
-      <div>Main body</div>
+      <div className='DescBig'>안녕하세요!</div>
+      <div className='DescBig'>프론트엔드 개발자 </div>
+      <div className='DescBig'>유재우 입니다.</div>
+      {/* add laning contents */}
       <CustomButton text="CustomButton"/>
       </>
 
