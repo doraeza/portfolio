0093c0b8e217255b21ddc49933034cc0e1a8fe9d
doraeza <pp8960@naver.com>
2023-06-25 15:56:00 +0900

    header1

--- portfolio/src/pages/App.tsx
+++ portfolio/src/pages/App.tsx
@@ -1,5 +1,4 @@
 import CustomButton from '../components/common/CustomButton';
-import BallIcon from '../components/common/ballIcon';
 import '../styles/globals.css';
 
 
@@ -10,7 +9,6 @@ function App() {
       <div className='DescBig'>안녕하세요!</div>
       <div className='DescBig'>프론트엔드 개발자 </div>
       <div className='DescBig'>유재우 입니다.</div>
-      <BallIcon />
       {/* add laning contents */}
       <CustomButton text="CustomButton"/>
       </>
