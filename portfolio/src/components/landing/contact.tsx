40c65a7126b47857bf1fd2b93b4b1ce64eee1650
doraeza <pp8960@naver.com>
2023-06-30 15:25:53 +0900

    landing contact8

--- portfolio/src/components/landing/contact.tsx
+++ portfolio/src/components/landing/contact.tsx
@@ -35,7 +35,7 @@ const Contact = () => {
   };
 
   return (
-    <div>
+    <div style={{background: "#191919", border:"solid 1px #9bd65cd6", padding: "5px", width: "350px"}}>
         <form onSubmit={handleSubmit}>
             <div>
                 <label style={{color: "#9bd65c", textShadow: "0 0 10px #9bd65c"}} htmlFor="name">이  름:</label>
@@ -43,18 +43,18 @@ const Contact = () => {
                 type="text" id="name" name="name" value={formData.name} onChange={handleInputChange} />
             </div>
 
-            <div>
+            <div style={{marginTop: "15px"}}>
                 <label style={{color: "#9bd65c", textShadow: "0 0 10px #9bd65c"}} htmlFor="email">이메일:</label>
                 <input style={{background: "#191919", border:"solid 1px #9bd65cd6", color: "#9bd65c", textShadow: "0 0 10px #9bd65c"}}
                 type="email" id="email" name="email" value={formData.email} onChange={handleInputChange} />
             </div>
 
-            <div>
+            <div style={{marginTop: "15px"}}>
                 <label style={{color: "#9bd65c", textShadow: "0 0 10px #9bd65c"}} htmlFor="message">메시지:</label>
                 <textarea style={{background: "#191919", border:"solid 1px #9bd65cd6", color: "#9bd65c", textShadow: "0 0 10px #9bd65c"}}
                 id="message" name="message" value={formData.message} onChange={handleInputChange} />
             </div>
-            <button style={{background: "#191919", cursor: "pointer", borderRadius: "9px", border:"solid 1px #9bd65cd6", color: "#9bd65c", textShadow: "0 0 10px #9bd65c"}} type="submit">전송</button>
+            <button style={{marginTop: "15px",background: "#191919", cursor: "pointer", borderRadius: "9px", border:"solid 1px #9bd65cd6", color: "#9bd65c", textShadow: "0 0 10px #9bd65c"}} type="submit">전송</button>
 
         </form>
     </div>
